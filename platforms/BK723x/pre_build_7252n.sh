ac25d2d4daef0916e81ceff99207973d623f58f7
Artem Sidenko <137616254+Imager0@users.noreply.github.com>
2026-06-25 05:08:15 +0300

    Update BLE configurations for OpenBK7252N
    Uncomment BLE role definitions and add missing CFG_BLE_HOST_RW.

--- platforms/BK723x/pre_build_7252n.sh
+++ platforms/BK723x/pre_build_7252n.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 echo "Applying BLE patches to Beken SDK for OpenBK7252N..."
 
-# Patch sys_config_bk7252n.h to enable BLE 5.2 (not 5.1!)
+# Patch sys_config_bk7252n.h to enable BLE 5.2 and required definitions
 sed -i 's/#define CFG_SUPPORT_BLE                            0/#define CFG_SUPPORT_BLE                            1/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
 sed -i 's/#define CFG_BLE_VERSION                            BLE_VERSION_4_2/#define CFG_BLE_VERSION                            BLE_VERSION_5_2/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+
+# Uncomment BLE role numbers which are commented out by default for 7252N
+sed -i 's/\/\/#define CFG_BLE_ADV_NUM/#define CFG_BLE_ADV_NUM/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+sed -i 's/\/\/#define CFG_BLE_SCAN_NUM/#define CFG_BLE_SCAN_NUM/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+sed -i 's/\/\/#define CFG_BLE_INIT_NUM/#define CFG_BLE_INIT_NUM/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+sed -i 's/\/\/#define CFG_BLE_CONN_NUM/#define CFG_BLE_CONN_NUM/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+sed -i 's/\/\/#define CFG_BLE_USE_DYN_RAM/#define CFG_BLE_USE_DYN_RAM/' sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+
+# Add CFG_BLE_HOST_RW which is entirely missing
+echo "#ifndef CFG_BLE_HOST_RW" >> sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+echo "#define CFG_BLE_HOST_RW                            1" >> sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
+echo "#endif" >> sdk/beken_freertos_sdk/beken378/app/config/sys_config_bk7252n.h
 
 # Patch ble_main.c for 5.2 to wrap FIQ_BTDM with #ifdef
 sed -i 's/intc_enable(FIQ_BTDM);/#ifdef FIQ_BTDM\n\t\tintc_enable(FIQ_BTDM);\n#endif/' sdk/beken_freertos_sdk/beken378/driver/ble/ble_5_2/platform/bk7252n/entry/ble_main.c
